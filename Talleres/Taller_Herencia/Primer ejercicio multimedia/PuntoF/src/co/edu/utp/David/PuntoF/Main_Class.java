b6125ccaad6e8a08c30a2de07ec26ff94a5923dd
david970218 <davidperez19940218@gmail.com>
2018-09-11 15:00:43 -0500

    Taller herencia

---
+++ Talleres/Taller_Herencia/Primer ejercicio multimedia/PuntoF/src/co/edu/utp/David/PuntoF/Main_Class.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.David.PuntoF;
+
+import co.edu.utp.David.Taller_Herencia_Multimedia.*;
+
+/**
+ *
+ * @author david_perez
+ */
+public class Main_Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ListaMultimedia lista;
+        lista = new ListaMultimedia(20);
+        //*************************************************************
+        lista.add(new Disco("Sonne","Rammstein","mp3","4:30 min"));
+        lista.add(new Disco("Molinos de viento","mago de oz","mp3","5:35 min"));
+        lista.add(new Disco("Survival","Muse","mp3","3:20 min"));
+        //***************************************************************
+        for (int i = 0; i < lista.Size(); i++)
+            System.out.println(lista.get(i));
+    }
+    
+}
